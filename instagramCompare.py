0ed5d3c7925c192680ba4683489dd6c4d24d159c
Isabelle Lee <isabellee.leee@gmail.com>
2026-04-24 16:53:29 -0700

    updated
    updated arrays

--- instagramCompare.py
+++ instagramCompare.py
@@ -15,16 +15,9 @@ followingList = []
 followerList = []
 notFollowingMeBack = []
 notFollowingThemBack = []
-okWithNot = ["4mybelly", "aztecblockchaingroup", "sdsuaiclub", "same.sdsu", "sdsugreenlove", "sdsu.gwc", "sdsugirlup", "sdsuleadandserve", 
-             "sdsu_aitp", "sdsu_eleetcoders", "sdsuapsirho", "cloud9gg", "lftoxy", "muelleresportsmedicine", "sdsugreenlove", "therivesandiego", "leadershipclubofsdsu",
-            "pibetaphisdsu", "sdsupanhellenic", "gammaphisdsu", "deltagammasdsu", "thetasdsu", "alphaphisdsu", "kappadeltasdsu", "axosdsu", "sdsuchio", 
-            "sstarb0iiii", "taesroy", "luciaaferrato", "2hollis", "buldak_global", "riovaz", "elizabrowne_", "kaniiberry", "ariana_greenblatt", "aimeejaihall", "roblox",
-            "joycee.e", "stxlkin", "bhertuy", "pinkpantheress", "heavnlyheavn", "absamahan", "internetgirlmusic", "eyebleachairdyed", "sundaykalogeras", "ihatebixby", 
-            "0noahh", "hereisalden", "ashbluegutierrez", "evan.kohli", "1glaive", "ericdoa", "valorant", "1endie", "starfallxo", "starfallxo", "scruff.hb", "re6ce", "svphietran"]
-deleted = ["mexican_crystals69", "akpineapple",  "shriyans_vittal", "pippers12344444445", "__xand3rr", "sergio760__", "marcelinesong_", "d_lo411", "__deleted__bhiebefgijhjcbfij", 
-           "matt.hulu", "subidowido", "celeste.cartland", "kayydencebellardoo", "audrey.melani_", "sdsuclass_28", "brandongiolzetti", "", "", ""]
-deactivated = ["tamsteroof", "kylessim", "mira_gandhi_", "sheldontan24", "nickvsalvatore", "averyhoangg", "mmonicaagil", "sososquali", "josephh.perez", "toren.rogers", "m_vargas619", 
-               "andreamalia.b", "kingj4wsh", "justjordanlou", "kaylawnuh__", "brandongiolzetti", "ha_.rry9537", "carranzalaila"]
+celebrities = []
+deleted = []
+deactivated = []
 
 #add users to my following list
 lines = file_following.readlines()
@@ -50,7 +43,7 @@ for line in lines:
 # compare lists (do not mutate while iterating)
 notFollowingThemBack = [x for x in followerList if x not in followingList]
 notFollowingMeBack = [x for x in followingList if x not in followerList]
-interest = [x for x in notFollowingMeBack if x not in okWithNot and x not in deleted and x not in deactivated]
+interest = [x for x in notFollowingMeBack if x not in celebrities and x not in deleted and x not in deactivated]
 
 print("Not following them back:")
 for x in notFollowingThemBack:
